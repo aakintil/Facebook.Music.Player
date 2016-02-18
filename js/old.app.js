0afe8e21c39b5236d10e2e82048061280b492721
Aderinsola <aderinsola_akintilo@ziba.com>
2016-02-18 15:22:33 -0800

    saving before this crashes

--- js/old.app.js
+++ js/old.app.js
@@ -1,9 +1,3 @@
-function debug( type ) {
-    var s = "** SUCCESS  ** \n (^.^) ", 
-        e = " -- ERROR -- \n (%_%) "; 
-    ( type === "ya" ) ? console.log( s ) : console.log( e ); 
-}
-
 function get_url( text ) {
     // regex to get url and only urls
     var urlRegex = /(https?:\/\/[^\s]+)/g;
@@ -29,7 +23,7 @@ $( document ).ready( function() {
     //	console.log( data.length ); 
 
     // placing and sorting data inside container
-    var container = $( '#archive .container' ); 
+    var container = $( '.header' ); 
 
     // loop through data
     var c = 0, 
@@ -39,13 +33,6 @@ $( document ).ready( function() {
             soundcloud: 0, 
             vevo: 0, 
             spotify: 0
-        }, 
-        musicData = { 
-            youtube: {}, 
-            vimeo: {}, 
-            soundcloud: {}, 
-            vevo: {}, 
-            spotify: {} 
         }, 
         count, 
         youtube_data = ""; 
@@ -57,49 +44,36 @@ $( document ).ready( function() {
     //        vevo = 'vevo', 
     //        spotify = 'spotify'; 
 
-    for ( var i = 0; i < 20; i++ ) {
+    for ( var i in data ) {
         if ( data[ i ].link !== undefined ) {
-            //            var message = ( data[ i ].message === undefined ) ? 'There are no messages for this post' : ( data[ i ].message ), 
-            //                link = ( data[ i ].link === undefined ) ? 'There are no links for this post': data[ i ].link, 
-            //                title = data[ i ].title; 
-
-            var message = "", 
-                link = "", 
-                title = ""; 
-
+            var message = ( data[ i ].message === undefined ) ? 'There are no messages for this post' : ( data[ i ].message );
+            var link = ( data[ i ].link === undefined ) ? 'There are no links for this post': data[ i ].link;
+            var title = data[ i ].title ; 
+            //            console.log( data[i].link );
+            //            if ( data[i].link.indexOf( 'youtube' ) !== -1 || data[i].link.indexOf( 'youtu.be' ) !== -1 ) {
+            //                count++; 
+            //            }
             var l = data[ i ].link; 
 
             if ( l.indexOf( 'youtube' ) !== -1 || l.indexOf( 'youtu.be' ) !== -1 ) {
                 music.youtube++; 
-                musicData.youtube[ c ] = data[ i ]; 
-
-                // storing only the youtube variables
-                message = ( data[ i ].message === undefined ) ? 'There are no messages for this post' : ( data[ i ].message );
-                link = ( data[ i ].link === undefined ) ? 'There are no links for this post': data[ i ].link; 
-                title = data[ i ].title; 
             }
             if ( l.indexOf( 'vimeo' ) !== -1 ) {
                 music.vimeo++; 
-                musicData.vimeo[ c ] = data[ i ]; 
             }
             if ( l.indexOf( 'soundcloud' ) !== -1 ) {
                 music.soundcloud++; 
-                musicData.soundcloud[ c ] = data[ i ]; 
             }
             if ( l.indexOf( 'vevo' ) !== -1 ) {
                 music.vevo++; 
-                musicData.vevo[ c ] = data[ i ]; 
             }
             if ( l.indexOf( 'spotify' ) !== -1 ) {
-                music.spotify++;
-                musicData.spotify[ c ] = data[ i ]; 
+                music.spotify++; 
             }
-
-
             var img = ( data[ i ].full_picture === undefined ) ? 'img/no.pic.png' : data[ i ].full_picture; 
             var date = moment( data[ i ].created_time ).format('MMMM Do YYYY, h:mm:ss a');
             var user = data[ i ].from.name; 
-            // moment().format('MMMM Do YYYY, h:mm:ss a'); // May 10th 2015, 3:15:09 pm
+            //			moment().format('MMMM Do YYYY, h:mm:ss a'); // May 10th 2015, 3:15:09 pm
             var block = $( '<div class="block">' 
                           + '<p class="message">' + message + '</p>'
                           + '<p class="link">' + link + '</p>'
@@ -108,12 +82,13 @@ $( document ).ready( function() {
                           + '<p class="date"> Posted: ' + date + '</p>'
                           + '<img class="picture" src="' + img + '"/>'
                           + '</div>' );
-            container.append( block ); 
+            //            container.append( block ); 
             c++; 
         }
     }
-
-    ( _.size( musicData.youtube ) > 0 ) ? debug( "ya") : debug( "fuck" );
+    //	console.log( "data count:  ", data.length, "  |  live link count:  ", c ); 
+    //    console.log( "youtube count:  ", count, "  |  live link count:  ", c ); 
+    console.log( " where we stand : ", music );
 
     // checkout this for npm
     // https://www.npmjs.com/package/play-url
