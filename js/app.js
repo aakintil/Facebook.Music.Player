726f3b90ad902eba813b357dc98c1af344ea62c7
Aderinsola <aderinsola_akintilo@ziba.com>
2016-02-18 17:55:46 -0800

    got songs with links showing up

--- js/app.js
+++ js/app.js
@@ -58,14 +58,14 @@ $( document ).ready( function() {
     //        spotify = 'spotify'; 
 
     for ( var i = 0; i < 20; i++ ) {
-        if ( data[ i ].link !== undefined ) {
+        if ( data[ i ].link !== undefined || data[ i ].source !== undefined ) {
             //            var message = ( data[ i ].message === undefined ) ? 'There are no messages for this post' : ( data[ i ].message ), 
             //                link = ( data[ i ].link === undefined ) ? 'There are no links for this post': data[ i ].link, 
             //                title = data[ i ].title; 
-
             var message = "", 
                 link = "", 
-                title = ""; 
+                source = "", 
+                post_name = ""; 
 
             var l = data[ i ].link; 
 
@@ -75,9 +75,11 @@ $( document ).ready( function() {
 
                 // storing only the youtube variables
                 message = ( data[ i ].message === undefined ) ? 'There are no messages for this post' : ( data[ i ].message );
-                link = ( data[ i ].link === undefined ) ? 'There are no links for this post': data[ i ].link; 
-                title = data[ i ].title; 
+                link = ( data[ i ].link === undefined ) ? 'null' : data[ i ].link; 
+                post_name = data[ i ].name; 
+                source = data[ i ].source; 
             }
+
             if ( l.indexOf( 'vimeo' ) !== -1 ) {
                 music.vimeo++; 
                 musicData.vimeo[ c ] = data[ i ]; 
@@ -96,23 +98,34 @@ $( document ).ready( function() {
             }
 
 
-            var img = ( data[ i ].full_picture === undefined ) ? 'img/no.pic.png' : data[ i ].full_picture; 
-            var date = moment( data[ i ].created_time ).format('MMMM Do YYYY, h:mm:ss a');
-            var user = data[ i ].from.name; 
+
+            var img = ( data[ i ].full_picture === undefined ) ? 'img/no.pic.png' : data[ i ].full_picture, 
+                date = moment( data[ i ].created_time ).format('MMMM Do YYYY, h:mm:ss a'), 
+                user = data[ i ].from.name, 
+                link_display = function( l ) {
+                    var link = ""; 
+                    ( l !== "null" ) ? link = '<a target="_blank" href="' + l + '" class="link"> Listen & Watch </a>' : link = ""; 
+                    return link; 
+                }
+
             // moment().format('MMMM Do YYYY, h:mm:ss a'); // May 10th 2015, 3:15:09 pm
-            var block = $( '<div class="block">' 
+            var block = $( '<div class="row">'
+                          + '<div class="block col-md-offset-3 col-md-6">' 
                           + '<p class="message">' + message + '</p>'
-                          + '<p class="link">' + link + '</p>'
-                          + '<p class="title">' + title + '</p>'
+                          + link_display( link ) 
+                          + '<p class="source">' + source + '</p>'
+                          + '<p class="title">' + post_name + '</p>'
                           + '<p class="user">' + user + '</p>'
                           + '<p class="date"> Posted: ' + date + '</p>'
                           + '<img class="picture" src="' + img + '"/>'
+                          + '</div>' 
                           + '</div>' );
             container.append( block ); 
             c++; 
         }
     }
 
+    // console.log( musicData.youtube[ 1 ] ); 
     ( _.size( musicData.youtube ) > 0 ) ? debug( "ya") : debug( "fuck" );
 
     // checkout this for npm
