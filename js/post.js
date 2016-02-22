c79f9f660c127e763ca646029fa24dbf164ad68c
Aderinsola <aderinsola_akintilo@ziba.com>
2016-02-22 11:58:50 -0800

    testing OO js

---
+++ js/post.js
@@ -0,0 +1,22 @@
+var post = {
+    name: "", 
+    url: "", 
+    platform: "", 
+    author: "", 
+    img: "", 
+    posted: "", 
+    songs_from_author: function( author ) {
+        
+    }
+}
+
+
+function Posts( post ) = {
+    this.post = post;
+    this.post_list = []; 
+    this.count = 
+}
+
+Posts.prototype.count = function() {
+    console.log( "called" );
+}
