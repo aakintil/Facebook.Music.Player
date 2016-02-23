aa74abeb2d4882b4de41e1fcc32a8552ea833b7e
Aderinsola <aderinsola_akintilo@ziba.com>
2016-02-23 13:40:59 -0800

    kinda got search working, but I need to redesign the layout

---
+++ js/search.filter.js
@@ -0,0 +1,50 @@
+// SOURCE:
+// http://www.robsawyer.me/blog/2013/07/10/lightweight-jquery-searchfilter-tutorial/
+
+$( document ).ready( function() {
+
+    // we want this function to fire whenever the user types in the search-box
+    $( "#search-posts" ).keyup( function () {
+
+        // first we create a variable for the value from the search-box
+        var searchTerm = $( "#search-posts" ).val();
+
+        // then a variable for the post items (to keep things clean)
+        var listItem = $( '#archive' ).find( '.row' ); 
+
+
+        // extends the default :contains functionality to be case insensitive
+        // if you want case sensitive search, just remove this next chunk
+        $.extend( $.expr[':'], {
+            'containsi': function( elem, i, match, array )
+            {
+                return ( elem.textContent || elem.innerText || '').toLowerCase()
+                    .indexOf( ( match[3] || "" ).toLowerCase() ) >= 0;
+            }
+        });
+        // end of case insensitive chunk
+
+
+        // this part is optional
+        // here we are replacing the spaces with another :contains
+        // what this does is to make the search less exact by searching all words and not full strings
+        var searchSplit = searchTerm.replace( / /g, "'):containsi('" )
+
+
+        // here is the meat. We are searching the list based on the search terms
+        $( listItem ).not( ":containsi('" + searchSplit + "')" ).each( function( e )   {
+
+            // add a "hidden" class that will remove the item from the list
+            $( this ).addClass( 'hidden' );
+
+        });
+
+        // this does the opposite -- brings items back into view
+        $( "#list li:containsi('" + searchSplit + "')" ).each( function( e ) {
+
+            //remove the hidden class (reintroduce the item to the list)
+            $( this ).removeClass( 'hidden' );
+
+        });
+    })
+}); 
