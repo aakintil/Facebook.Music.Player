fe073689514d19cd1aac6abc528f86eb4e0e5b7c
Aderinsola <aderinsola_akintilo@ziba.com>
2016-02-22 17:45:57 -0800

    making a really nice class for posts and shit

--- js/post.js
+++ js/post.js
@@ -43,6 +43,10 @@ Authors.prototype.clearAuthors = function() {
     this.list = []; 
 }
 
+Authors.prototype.removeAuthor = function( author ) {
+
+}
+
 Authors.prototype.consolidateAuthors = function( posts ) {
 
     var temp = []; 
@@ -53,7 +57,6 @@ Authors.prototype.consolidateAuthors = function( posts ) {
 
         // loop through and find where else the author exists
         for ( var j = 0; j < posts.length; j++ ) {
-
             // if the name are the same, consolidate
             if ( posts[ j ].author === i ) {
                 a.addOnePost( posts[ j ] ); 
@@ -63,8 +66,44 @@ Authors.prototype.consolidateAuthors = function( posts ) {
         // add authors to temp object
         temp.push( a ); 
     }
+
+    // check for authors with 0 posts + 
     // eliminate temp and set this.list back to main node
-    this.list = temp; 
+    this.list = minusZeroes( temp )
+}
+
+Authors.prototype.each = function() {
+    for ( var i in this.list ) {
+        console.log( "Author:Posts -->   ", this.list[ i ].name, ":", this.list[ i ].posts.length ); 
+    }
+}
+
+Authors.prototype.addAllToDOM = function() {
+    var author_block = $( '<div class="col-md-3"></div>' ), 
+        _author = $( "<div></div>" ); 
+
+    for ( var i in this.list ) {
+        var author = this.list[ i ], 
+            name = author.name, 
+            post_count = author.posts.length, 
+            obj = $( '<p> <span class="author">' + name + '</span> <span class="postsNum">' + post_count + '</span> </p>' ); 
+        _author.append( obj ); 
+    }
+
+    // adding to the DOM 
+    author_block.append( _author ); 
+    return author_block; 
+}
+
+function minusZeroes( temp ) {
+    var minus_zeroes = []; 
+    for ( var i in temp ) {
+        if ( temp[ i ].posts.length === 0 ) {
+            minus_zeroes = _.reject( temp, function( author ){ return author.posts.length === 0 });
+        }    
+    }
+
+    return minus_zeroes; 
 }
 
 
@@ -97,17 +136,22 @@ Author.prototype.addToPostIdArray = function( id ) {
 
 Author.prototype.addPosts = function( post ) {
     for ( var i in post ) {
-        this.posts.push( post[ i ] ); 
+        if ( post[ i ].url !== undefined )
+            this.posts.push( post[ i ] ); 
     }
 }
 
 Author.prototype.addOnePost = function( post ) {
-    this.posts.push( post ); 
+    if ( post.url !== undefined )
+        this.posts.push( post ); 
+    //    else 
+    // console.log( "cannot add post: the url is not provided" ); 
 }
 
 Author.prototype.removeOnePost = function( post ) {
     //    this.posts.push( post ); 
 }
+
 
 
 
@@ -122,8 +166,8 @@ function Post( data ) {
     this.name = data.name;  
     this.url = data.link || data.source; 
     this.platform = findPlatform( data );  
-    this.author = data.from.name; 
-    this.img = data.full_picture || ""; 
+    this.author = data.from.name; // or the actual object
+    this.img = data.full_picture || 'img/no.pic.png'; 
     this.posted = moment( data.created_time ).format('MMMM Do YYYY, h:mm:ss a'); 
     this.likes = 0 //|| data.likes.data.length; 
 
@@ -176,6 +220,8 @@ Post.prototype.getAuthor = function() { return this.author; }
 
 Post.prototype.getPlatform = function() { return this.platform; }
 
+Post.prototype.setAuthor = function( author )  { this.author = author }; 
+
 
 
 /////////////////////
@@ -212,7 +258,15 @@ Posts.prototype.calculateAuthorCount = function() {
 }
 
 Posts.prototype.addPost = function( post ) {
-    this.list.push( post ); 
+    if ( hasLink( post ))
+        this.list.push( post ); 
+}
+
+function hasLink( post ) {
+    if ( post.url !== undefined ) { 
+        return true;
+    }
+    return false; 
 }
 
 Posts.prototype.removePost = function( post ) {
@@ -232,35 +286,82 @@ Posts.prototype.setList = function( new_list ) {
     this.list = new_list; 
 }
 
-Posts.prototype.addAllToDOM = function() {
-    console.log( $( 'body' ) ); 
+Posts.prototype.addAllToDOM = function( authors ) {
+    // grab the container
+    var container = $( '#archive .main.container' ), 
+        post = "", 
+        row_num = 0; 
+    for ( var i in this.list ) {
+        post = this.list[ i ]
+        // storing all data into appr. attributes
+        var url = post.url, 
+            post_name = post.name,  
+            // source = data[ i ].source; 
+            img = post.img, 
+            date = post.posted, 
+            user = post.author.name; 
+
+        // create the music post container
+        // the first row has to support the side nav for authors. that's it
+        var block_row = ( row_num === 0 ) ? $( '<div class="row">'
+                                                    + '<div class="block col-md-6">' 
+                                                    + '</div>' 
+                                                    + '</div>' ) 
+        : $( '<div class="row">'
+            + '<div class="block col-md-offset-3 col-md-6">' 
+            + '</div>' 
+            + '</div>' );
+
+        // checking to see if any of the fields are null
+        block_row.find( '.block').append( '<p class="title">' + post_name + '</p>' ); 
+        block_row.find( '.block').append( '<p class="user">' + user + '</p>' ); 
+        block_row.find( '.block').append( '<p class="date"> Posted: ' + date + '</p>' ); 
+        // block_row.find( '.block').append( '<p class="message">' + message + '</p>' ); 
+        block_row.find( '.block').append( url ); 
+        // block_row.find( '.block').append( '<p class="source">' + source + '</p>' ): do_nothing() ; 
+        block_row.find( '.block').append( '<img class="picture" src="' + img + '"/>' ); 
+
+        container.append( block_row ); 
+        row_num++; 
+    }
+    // personal count for # of users...maybe even sources? 
+    //( _.size( musicData.youtube ) > 0 ) ? debug( "ya") : debug( "fuck" );
+
+    console.log( authors )
+    // adding the authors 
+    
+    container.find( '.row:first-child' ).prepend( authors.addAllToDOM() ); 
 }
 
 Posts.prototype.addOneToDOM = function( post ) {
 
 }
 
+var data = save_my_inbox.data, 
+    authors = new Authors( data ),
+    posts = new Posts(); 
 
 function init() {
-    data = save_my_inbox.data, 
-        authors = new Authors( data ), 
-        posts = new Posts(); 
+    var post = "", 
+        author = ""; 
 
     for ( var i in data ) {
-        var p = new Post( data[ i ] ); 
-        var a = new Author( data[ i ].from.name ); 
-        a.addOnePost( p );
-        a.internalInitPostIdArray( p.id ); 
-        posts.addPost( p ); 
+        post = new Post( data[ i ] ); 
+        author = new Author( data[ i ].from.name ); // have to find a way to consolidate this. right now it's pulling from a very different data set. hence creating the Author with 0 posts issue.  
+        author.addOnePost( post );
+        post.setAuthor = author; 
+        author.internalInitPostIdArray( post.id ); 
+        posts.addPost( post ); 
     }
 
     // need this to make turn the duplicate list into more of a hash table
     authors.consolidateAuthors( posts.list );
-    console.log( authors.list[ 0 ] ); 
 }
 init(); 
 
-posts.addAllToDOM();
+posts.authors = authors.list; 
+//authors.each(); 
+posts.addAllToDOM( authors );
 
 // post is working
 // p = new Post( save_my_inbox.data[ 0 ] ); 
